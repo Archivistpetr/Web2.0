917c34b018223cd7e4a022497816b973d79b0f2c
Archivistpetr <151119925+Archivistpetr@users.noreply.github.com>
2023-11-21 14:05:25 -0800

    Delete dist directory

--- dist/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to toggle the menu on click
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    const menu = document.querySelector('.menu');
-
-    menuIcon.addEventListener('click', function () {
-        menu.classList.toggle('show-menu');
-    });
-});
